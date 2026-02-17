c6193c987dff1489bf1d6c5ab183fe6d8a9f9839
botirbakhtiyarov <botirbakhtiyarovb@gmail.com>
2026-02-17 14:57:43 +0800

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,6 @@
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
-# create superuser only if not exists
-python manage.py shell -c "
-from django.contrib.auth import get_user_model;
-import os;
-User=get_user_model();
-u=os.environ.get('DJANGO_SUPERUSER_USERNAME');
-p=os.environ.get('DJANGO_SUPERUSER_PASSWORD');
-e=os.environ.get('DJANGO_SUPERUSER_EMAIL');
-if u and p and not User.objects.filter(username=u).exists():
-    User.objects.create_superuser(u,e,p)
-"
+python create_superuser.py
+
 exec "$@"
