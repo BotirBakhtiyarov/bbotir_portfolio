5f216cd661bb8e82b28586266e013be396b609e9
botirbakhtiyarov <botirbakhtiyarovb@gmail.com>
2026-02-17 14:45:10 +0800

    commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python manage.py collectstatic --noinput
-python manage.py migrate portfolio --noinput
+python manage.py migrate --noinput
 
 # create superuser only if not exists
 python create_superuser.py
